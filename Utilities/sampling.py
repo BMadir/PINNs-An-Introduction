6a8f705ca7ab425cea77d8d6a08adf5dc24fde61
BMadir <madir.bahaedine@gmail.com>
2026-06-23 18:34:03 +0200

    Sampling utilities

--- Utilities/sampling.py
+++ Utilities/sampling.py
@@ -1,3 +1,20 @@
+"""
+Sampling and adaptive Sampling for PINNs
+
+This module provides:
+- Sampler: Latin hypercube / Sobol / Halton / Hammersley sampling in bounded domains
+- Join_samplers: combination of multiple sampling strategies with weighted proportions
+- Adaptor: residual-based adaptive refinement (RAR) and density-based sampling (RAD)
+- Data_sampler: sampling from fixed datasets with optional importance weighting
+
+Key features:
+- Supports NumPy and PyTorch outputs
+- Adaptive sampling based on model residuals
+- Integration with PINNs and PDE training pipelines
+- Flexible composition of multiple sampling strategies
+"""
+
+
 import numpy as np
 import torch
 import skopt
@@ -35,32 +52,6 @@ def _rad_fn(sample_fn, func, num_in, num_out, k, c, seed=1234, **kwargs):
         indices = indices.flatten()
         return *[s[indices] for s in sample], output[indices]
 
-
-"""
-def _rad_fn(sample_fn, func, num_in, num_out, k, c, in_dim=None, seed=1234, **kwargs):
-
-    r = num_out/num_in
-    coef = 1/(r* np.log(2* k + np.log(1 + r)* c))
-    
-    ''' Residual-based adaptive distribution '''
-    # generator = torch.Generator("cuda").manual_seed(seed)
-    try:
-        generator = kwargs.pop("generator")
-    except:
-        generator = None
-    sample = sample_fn(num_in, **kwargs)
-    if in_dim is None:
-        in_dim = len(sample) - 1
-    inputs, outputs = sample[:in_dim], sample[in_dim:]
-    abs_value = func(*inputs).abs().flatten()
-    with torch.no_grad():
-        density = (abs_value ** k) / (abs_value ** k).mean() + c
-        density /= density.sum()
-        indices = torch.multinomial(density, num_out, generator=generator)
-        indices = indices.flatten()
-        #return tuple(s[indices] for s in sample)
-        return *[s[indices] for s in sample], coef* density[indices].reshape(-1, 1)
-"""
 
 ### Sampler class
 class Sampler:
@@ -149,16 +140,8 @@ class Join_samplers:
                     samples[i] = np.vstack([samples[i], s])
         return tuple(samples)
 
-# Adapt a sample function
+### Adapt a sample function
 class Adaptor:
-    """ Adapt a given sampler to acordin to a certain function.
-
-       Args:
-           method: RAR or RAD.
-           sample_fn:
-           func:
-           num_in=None:
-        """
     def __init__(self, method, sample_fn, func, num_in=None, seed=1234, **adaptor_kwargs):
         sample_kwargs = copy.deepcopy(adaptor_kwargs)
         if method.lower() == "rad":
@@ -190,37 +173,6 @@ class Adaptor:
             return self._rar_fn(num_out=n)
         elif self.method.lower() == "rad":
             return self._rad_fn(num_out=n)
-
-    """ To review
-    def _update(self, sample, set_2=False, set_1_size=None, save=False):
-        if self.method.lower() == "rar":
-            adapted = self.rar_fn()
-        elif self.method.lower() == "rad":
-            adapted = self.rad_fn()
-        else:
-            raise Exception("unsupported adaptor")
-        if save:
-            self._save(adapted)
-        if set_2 and set_1_size:
-            return [torch.vstack([s[0:set_1_size, :], adapted[i]]) for i, s in enumerate(sample)]
-        else:
-            return adapted
-
-    def _save(self, sample):
-        if isinstance(sample, list):
-            pass
-        else:
-            sample = list(sample)
-
-        if self.state_dict is None:
-            keys = ["sample_" + str(i) for i in range(len(sample))]
-            values = [[] for i in range(len(sample))]
-            self.state_dict = dict(zip(keys, values))
-
-        num = len(sample)
-        for i in range(num):
-            self.state_dict["sample_" + str(i)].append(sample[i].detach().cpu())
-    """
 
 ### Sample from data
 class Data_sampler:
@@ -254,67 +206,3 @@ class Data_sampler:
         n = min(n, self.n)
         idx = torch.multinomial(self.density, n)
         return tuple(s[idx] for s in sample)
-
-""" To review
-class SeqToSeq(Sampler):
-    def __init__(self, n_seq, n_col, n_iter, l_bounds, u_bounds, func=None, sampler='lhs', **kwargs):
-        super().__init__(l_bounds, u_bounds, func, sampler, **kwargs)
-        self.n_seq = n_seq
-        self.n_col = n_col
-        self.seq_bounds = self._seq_bounds(n_seq, self.bounds, 0)
-        self.seq_sizes = self._seq_sizes(n_seq, n_col)
-
-        self.n_iter = n_iter
-        iterations = [n_iter for _ in self.seq_sizes]
-        self.iterations = [sum(iterations[:i]) for i in range(len(iterations))]
-
-        self.samples = self._samples(self.seq_sizes)
-
-    def _seq_bounds(self, n_seq, bounds, indice):
-        seq = lambda k: bounds[0][indice] + k * (bounds[1][indice] - bounds[0][indice]) / n_seq
-        seq_bounds = []
-        for k in range(1, n_seq + 1):
-            _bounds = bounds[1][:]
-            _bounds[indice] = seq(k)
-            _bounds = [bounds[0], _bounds]
-            seq_bounds.append(_bounds)
-        return seq_bounds
-
-    def _seq_sizes(self, n_seq, size):
-        size_0 = int(0.1 * size)
-        size_k = int((size - size_0) / (n_seq - 1)) + 1
-        tot = size_0 + (n_seq - 1) * size_k
-        size_0 = size_0 - (tot - size)
-        return [size_0 + k * size_k for k in range(n_seq)]
-
-    def __scale(self, sample, bounds):
-        dim = len(bounds[0])
-        lower = np.broadcast_to(bounds[0], dim)
-        upper = np.broadcast_to(bounds[1], dim)
-        return sample * (upper - lower) + lower
-
-    def __sample(self, n, bounds, tensor=True, device='cpu'):
-        x = self.sampler.generate(dimensions=self.unit_sq, n_samples=n, random_state=self.seed)
-        x = np.array(x)
-        x = self.__scale(x, bounds)
-        x = np.split(x, self.dim, 1)
-
-        if isinstance(self.func, list):
-            sample = *x, *[f(*x) for f in self.func]
-        else:
-            sample = *x, self.func(*x)
-
-        if tensor:
-            return self._Array_to_Tensor(sample, device)
-        else:
-            return sample
-
-    def _samples(self, seq_sizes):
-        samples = []
-        for (size, bounds) in zip(seq_sizes, self.seq_bounds):
-            sample = self.__sample(size, bounds, device="cuda")
-            samples.append(sample)
-
-        self.samples = samples
-        return samples
-"""
